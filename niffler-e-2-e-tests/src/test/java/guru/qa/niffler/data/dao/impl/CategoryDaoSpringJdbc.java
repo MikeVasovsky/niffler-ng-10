bce446493ffb67e1e0e7db639bef2fb73d485114
krylov_ilya <pptestkrylov@gmail.com>
2025-11-04 09:25:56 +0300

    Задание 5-1

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/impl/CategoryDaoSpringJdbc.java
@@ -0,0 +1,63 @@
+package guru.qa.niffler.data.dao.impl;
+
+import guru.qa.niffler.data.dao.CategoryDao;
+import guru.qa.niffler.data.entity.auth.AuthUserEntity;
+import guru.qa.niffler.data.entity.spend.CategoryEntity;
+import guru.qa.niffler.data.mapper.AuthUserEntityRowMapper;
+import guru.qa.niffler.data.mapper.CategoryEntityRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Optional;
+import java.util.UUID;
+
+public class CategoryDaoJdbc implements CategoryDao {
+
+  private DataSource dataSource;
+
+  public CategoryDaoJdbc(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  @Override
+  public CategoryEntity create(CategoryEntity category) {
+    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+    KeyHolder kh = new GeneratedKeyHolder();
+    jdbcTemplate.update(con -> {
+      PreparedStatement ps = con.prepareStatement(
+              "INSERT INTO \"category\" (name, username, archived) " +
+                      "VALUES (?, ?, ?)",
+              Statement.RETURN_GENERATED_KEYS
+    );
+      ps.setString(1,category.getName());
+      ps.setString(2, category.getUsername());
+      ps.setBoolean(3, category.isArchived());
+      return ps;
+  }, kh);
+    final UUID generatedKey = (UUID) kh.getKeys().get("id");
+    category.setId(generatedKey);
+    return category;
+  }
+
+  @Override
+  public Optional<CategoryEntity> findCategoryById(UUID id) {
+    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+    return Optional.ofNullable(
+            jdbcTemplate.queryForObject(
+                    "SELECT * FROM \"category\" WHERE id = ?",
+                    CategoryEntityRowMapper.instance,
+                    id
+            )
+    );
+  }
+
+
+  @Override
+  public CategoryEntity update(CategoryEntity categoryEntity) {
+    return null;
+  }
+}
